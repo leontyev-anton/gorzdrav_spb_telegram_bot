de670738dafeb5fe16dff8cbe844461b01e7d928
leontyev-anton <56308683+leontyev-anton@users.noreply.github.com>
2020-12-23 15:28:21 +0300

    Delete gorzdrav_email.sh

--- gorzdrav_email.sh
+++
@@ -1,5 +0,0 @@
-sleep 7 #чтобы не было в одно время с другими возможными ботами
-cd /home/Dropbox/PycharmProjects/gorzdrav/ #папка куда положили скрипт
-current_date=$(date +"%Y-%m-%d")
-filename='logs/'$current_date'.log'
-python3 gorzdrav_email.py >> $filename
