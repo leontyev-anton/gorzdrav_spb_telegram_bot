a3ea5d390a8ecf7ab6c7efacc0ab8cd787bdd906
leontyev-anton <56308683+leontyev-anton@users.noreply.github.com>
2021-09-22 17:49:38 +0300

    Update update_doctors_specialities.py

--- update_doctors_specialities.py
+++ update_doctors_specialities.py
@@ -30,7 +30,7 @@ def send_email(text):
     except Exception as e:
         log('Email notification was not sent - error with email sending: ' + str(e) )
 
-my_headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36',
+my_headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36',
               'Referer': 'https://gorzdrav.spb.ru/service-free-schedule',
               'X-Requested-With': 'XMLHttpRequest'}  # чтобы не выглядеть как бот, будем отправлять запросы с этими заголовками
 
@@ -38,35 +38,49 @@ datetime_now = datetime.utcnow() + timedelta(hours=3)
 log('Start:  ' + datetime_now.strftime('%Y-%m-%d %H:%M:%S' + '. '), end='')
 pg = create_engine('postgresql://' + pg_user + ':' + pg_pass + '@' + pg_host + '/' + pg_db)
 try:
-    specialities = pandas.read_sql(sql='SELECT speciality_id, name, lpu_id FROM specialities', con=pg)
+    #specialities = pandas.read_sql(sql="SELECT speciality_id, name, lpu_id FROM specialities WHERE speciality_id='2'", con=pg)
+    specialities = pandas.read_sql(sql="SELECT speciality_id, name, lpu_id FROM specialities", con=pg)
 except Exception as e:
     log(f'Exit script. Error requesting specialities from Postgres: {e}', admin=True)
 else:
     df_doctors = pandas.DataFrame()
     for i in specialities.index:
         #if specialities['speciality_id'][i] == '2' or specialities['speciality_id'][i] == '8' or specialities['speciality_id'][i] == '19':  # 19 - с нулем докторов
-        url = 'https://gorzdrav.spb.ru/_api/api/lpu/' + specialities['lpu_id'][i] + '/doctor?specialityId=' + specialities['speciality_id'][i]
-        try:
-            response = requests.get(url, headers=my_headers)
-            json = response.json()['result']
-        except Exception as e:
-            log(f'Exit script. Error parsing from JSON: {e}, url={url}', admin=True)
-            sys.exit(1)
-        else:
+        url = 'https://gorzdrav.spb.ru/_api/api/v2/schedule/lpu/' + specialities['lpu_id'][i] + '/speciality/' + specialities['speciality_id'][i] + '/doctors'
+        repeat = True   # сделаем 3 попытки на запрос
+        iterations = 0
+        while repeat:
             try:
-                df_doctor = pandas.DataFrame.from_dict(json)
-                df_doctor.drop(['areaNumber', 'areaType', 'freeParticipantCount', 'freeTicketCount', 'lastDate', 'nearestDate'], inplace=True, axis=1)
-            except:
-                pass
-            else:
-                df_doctor['speciality_id'] = specialities['speciality_id'][i]
-                df_doctor['speciality_name'] = specialities['name'][i]
-                df_doctor['lpu_id'] = specialities['lpu_id'][i]
-                df_doctors = df_doctors.append(df_doctor, ignore_index=True)
-                time.sleep(1)
+                iterations += 1
+                response = requests.get(url, headers=my_headers)
+                success = response.json()['success']
+                message = ''; exceptionMessage = ''
+                if success == False:
+                    message = response.json()['message']
+                    exceptionMessage = response.json()['exceptionMessage']
+                json = response.json()['result']
+                repeat = False
+            except Exception as e:
+                if iterations == 3:
+                    log(f'Exit script. Error parsing from JSON: {e}, after {iterations} iterations, exceptionMessages: '
+                        f'{message} {exceptionMessage}, url[{i}] = {url}', admin=True)
+                    sys.exit(1)
+                else:
+                    time.sleep(10 * iterations)
+        try:
+            df_doctor = pandas.DataFrame.from_dict(json)
+            df_doctor.drop(['ariaNumber', 'ariaType', 'freeParticipantCount', 'freeTicketCount', 'lastDate', 'nearestDate'], inplace=True, axis=1)
+        except:
+            pass
+        else:
+            df_doctor['speciality_id'] = specialities['speciality_id'][i]
+            df_doctor['speciality_name'] = specialities['name'][i]
+            df_doctor['lpu_id'] = specialities['lpu_id'][i]
+            df_doctors = df_doctors.append(df_doctor, ignore_index=True)
+            time.sleep(5)
 
 
-    if len(df_doctors) >= 47 and len(df_doctors) <= 55:
+    if len(df_doctors) >= 46 and len(df_doctors) <= 57:
         try:
             df_doctors = df_doctors.rename(columns={'id': 'doctor_id'})
             df_doctors.to_sql(name='doctors', con=pg, if_exists='replace', index=False)
@@ -75,57 +89,7 @@ else:
         else:
             log(f'{len(df_doctors)} doctors are successfully updated in Postgres')
             datetime_now = datetime.utcnow() + timedelta(hours=3)
-            log('Finish:  ' + datetime_now.strftime('%Y-%m-%d %H:%M:%S' + '. '), end='')
+            log('Finish:  ' + datetime_now.strftime('%Y-%m-%d %H:%M:%S' + '. '))
     else:
-        log(f'Exit script. Doctors less then 51 or more 55: {len(df_doctors)}. No update Postgres', admin=True)
+        log(f'Exit script. Doctors less then 46 or more 57: {len(df_doctors)}. No update Postgres', admin=True)
 
-
-# DROP TABLE IF EXISTS records
-#
-# CREATE TABLE records
-# (
-#    lpu_id TEXT,
-#    speciality_id TEXT,
-#    doctor_id  TEXT,
-#    notification_days INT,
-#    chat_id INT,
-#    date_creating TIMESTAMP,
-#    date_deleting TIMESTAMP,
-#    record_id SERIAL PRIMARY KEY
-# );
-#
-# INSERT INTO records (lpu_id, speciality_id, doctor_id, notification_days, chat_id, date_creating, date_deleting)
-#    VALUES ('147', '8', NULL, 10, '95453211', CAST('2020-11-19' AS TIMESTAMP), NULL);
-#
-# INSERT INTO records (lpu_id, speciality_id, doctor_id, notification_days, chat_id, date_creating, date_deleting)
-#    VALUES ('147','52', '311', 10, '95453211', CAST('2020-11-19' AS TIMESTAMP), NULL);
-
-
-
-
-
-# CREATE TRIGGER update_specialities AFTER UPDATE ON specialities
-#     FOR EACH ROW --WHEN (OLD.name IS DISTINCT FROM NEW.name)
-#     EXECUTE PROCEDURE update_specialities();
-#
-# CREATE OR REPLACE FUNCTION update_specialities() RETURNS trigger AS $$
-# BEGIN
-#    UPDATE records
-#       SET speciality_name = CONCAT(NEW.name,'1')
-#    WHERE records.speciality_id = NEW.speciality_id;
-#    RETURN NEW;
-# END;
-# $$ LANGUAGE  plpgsql;
-#
-# DROP FUNCTION update_specialities() CASCADE;
-#
-# UPDATE specialities SET name = 'Гастроэнтерл' WHERE speciality_id='8';
-#
-# SELECT EXISTS (
-#     SELECT  tgenabled
-#     FROM    pg_trigger
-#     WHERE   tgname='update_specialities' --AND
-#             --tgenabled != 'D'
-# );
-#
-# DELETE FROM specialities WHERE 1=1;
